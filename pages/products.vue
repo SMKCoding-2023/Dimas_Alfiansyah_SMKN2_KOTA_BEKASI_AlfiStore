536ae2443ad760417c518a14d445bab4e12b57fe
raaffiy <raafimaulana43@gmail.com>
2023-11-14 04:04:53 +0700

    update create product with api

--- pages/products.vue
+++ pages/products.vue
@@ -1,32 +1,58 @@
 <script lang="ts" setup>
-import { products } from "~/composables/constants/products";
+import { useProductsStore } from "~/stores/products";
+
+const productStore = useProductsStore();
+const allProducts = ref([]);
+
+productStore.getAllProducts().then(() => {
+  allProducts.value = productStore.products;
+});
+
 const selectedCategory = ref("");
-const allProducts = computed(() => {
-  if (selectedCategory.value) {
-    return products.filter((item) => item.category === selectedCategory.value);
+
+const categoryStore = useCategoryStore();
+const { categories } = storeToRefs(categoryStore);
+categoryStore.getAllCategory();
+
+const sortListProduct = computed(() => {
+  if (!selectedCategory.value) {
+    return allProducts.value;
+  } else {
+    // ignore the fkin error here !
+    return allProducts.value.filter(
+      (product) => product.category === selectedCategory.value
+    );
   }
-  return products;
 });
 </script>
 <template>
   <section>
     <div class="container">
       <div class="py-10">
-        <div class="mb-8 flex justify-end gap-6">
+        <div class="mb-8 flex justify-end gap-6 pr-10">
           <NuxtLink
             to="/category/create"
             class="bg-orange-500 text-white flex justifycenter items-center px-3 rounded-lg"
             >Create Category</NuxtLink
           >
+          <NuxtLink
+            to="/product/create"
+            class="bg-green-500 text-white flex justify-center items-center px-3 rounded-lg"
+            >Create Products</NuxtLink
+          >
           <Dropdown @selected-category="selectedCategory = $event" />
         </div>
+        <br />
+        <br />
         <div class="flex gap-10 flex-wrap ml-28">
-          <template v-for="(item, index) in allProducts" :key="index">
+          <template v-for="(item, index) in sortListProduct" :key="index">
             <CardsCardProduct :product="item" class="w-[calc(83%/4)]" />
           </template>
         </div>
       </div>
     </div>
+    <br />
+    <br />
     <br />
     <br />
     <br />
